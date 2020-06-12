6028a86eef54f47fd600f2aeba74afbfdfeb7e74
WilliamSousa1996 <Williamrsousa96@gmail.com>
2020-06-12 15:57:43 -0100

    exer4

---
+++ exer4.js
@@ -0,0 +1,7 @@
+//exercicio 4
+var velocidade = 180;
+if (velocidade > 80){
+    console.log("Esta acima da velocidade maxima permitida")
+}else{
+    console.log("Esta dentro do limite de velocidade")
+}
