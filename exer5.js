f855e067556f1b973bc011bc272efc526af8e84c
WilliamSousa1996 <Williamrsousa96@gmail.com>
2020-06-12 16:23:42 -0100

    exer5

---
+++ exer5.js
@@ -0,0 +1,10 @@
+//exercicio 5
+var idade = 20; 
+var cartaConducao = 0;
+if (idade>18 && cartaConducao==0){
+    console.log("Nao pode dirigir")
+} else if (idade>18 && cartaConducao==1){
+console.log("Pode dirigir")
+} else {
+    console.log("nao pode dirigir")
+}
