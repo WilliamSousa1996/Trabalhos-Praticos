93b9ac454efa4b69f8ae11cd90f729a13e911d00
WilliamSousa1996 <Williamrsousa96@gmail.com>
2020-06-12 16:45:36 -0100

    exer6

---
+++ exer6.js
@@ -0,0 +1,13 @@
+var idade = 22;
+var cartaConducao = 1;
+
+switch(idade>18 && cartaConducao == 0) {
+  case x:
+    console.log ("Nao pode dirigir");
+    break;
+  case y:
+    console.log ("Pode dirigir")
+    break;
+  default:
+    // code block
+}
